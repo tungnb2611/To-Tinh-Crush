8cb76c060c5357c72f68dba2959acefe86e9b0a1
tungnb2611 <62040289+tungnb2611@users.noreply.github.com>
2021-07-13 20:58:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Chan Thi Than à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `나는 당신에게 당신을 위해 내 모든 사랑을 보유하고 CO 선물을 보냈습니다. 
+    그러나 우체부가 나를 도울 수 없습니다, 그는 말했다 : “그것은 너무 큰 아들입니다”.`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Hết Giận Anh Nhé Em Yêu ',
+    desc: 'Cô gái anh yêu cái nhìn đầu tiên ',
+    btnYes: 'Yêu Anh <33',
+    btnNo: 'Không, Nhé Thằng kia  :3',
+    question:'Gửi cho anh bao nụ hôn thì ghi vào bên dưới nhé <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘, Cảm ơn em đã đến bên anh <3',
+    messDesc: 'Call cho anh luôn nhớ, Nhớ em quá ak <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://drive.google.com/file/d/1rTePrM_8_JJNWYbxGUrtXkyDnjP9goac/view?usp=sharing' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
